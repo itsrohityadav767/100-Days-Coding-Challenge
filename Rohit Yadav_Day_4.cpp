28d6e6112e6cf5b73ad4811ae0e8473f2b1e8d8e
Rohit Yadav <85444558+itsrohityadav767@users.noreply.github.com>
2023-01-26 10:29:41 +0530

    Add files via upload

---
+++ Rohit Yadav_Day_4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"enter the number to find ";
+	cin>>num;
+	if(num<0){
+		cout<<"Negative ";
+	}
+	else if(num>0)
+	cout<<"Positive";
+	
+	else
+	cout<<"Nor Negative Or Positive";
+return 0;
+}
