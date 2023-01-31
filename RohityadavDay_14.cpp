6d5e051ae9e5172f00aafe67943ebf893d55d7ef
Rohit Yadav <85444558+itsrohityadav767@users.noreply.github.com>
2023-01-31 18:54:29 +0530

    WAP to print reverse integer value?

---
+++ RohityadavDay_14.cpp
@@ -0,0 +1,17 @@
+// WAP to print reverse integer value?
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,rev=0;
+	 
+	 cout<<" Enter the value "<<endl;
+	 cin>>n;
+	 
+	 while(n>0){
+	 	rev=(rev*10)+n%10;
+	 	n=n/10;
+	 }
+	 cout<<rev;
+}
