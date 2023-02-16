f22828689e904577f07fcd5e3d10e3a3c55486e6
Rohit Yadav <85444558+itsrohityadav767@users.noreply.github.com>
2023-02-16 18:29:01 +0530

    Add files via upload

---
+++ RohityadavDay_29.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string str;
+	string str1;
+	cout<<"Enter the first string ";
+	cin>>str;
+	cout<<" Enter the second string ";
+	cin>>str1;
+	
+	cout<<str+str1;
+}
